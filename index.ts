7d4ad9ea44fb61ba82d756f0a160a36a63b995f6
Brandon McCraw <brandon.mccraw@provisionsgroup.com>
2022-07-23 21:25:01 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,54 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as awsx from "@pulumi/awsx";
+import { getListenerOutput, getLoadBalancerOutput } from "@pulumi/aws/alb";
+
+// Create a load balancer to listen for requests and route them to the container.
+const listener = new awsx.elasticloadbalancingv2.NetworkListener("nginx", { 
+    port: 80,
+});
+
+// const alb = getLoadBalancerOutput({
+//     arn: 'arn:aws:elasticloadbalancing:us-east-1:303658717825:loadbalancer/app/cust-api-alb/ff89ddfab088b4c0'
+// }
+// );
+
+// const listener_443 = getListenerOutput({
+//     arn: 'arn:aws:elasticloadbalancing:us-east-1:303658717825:listener/app/cust-api-alb/ff89ddfab088b4c0/85a78e728689e7ed',
+//     loadBalancerArn: alb.arn
+// });
+// const listener_80 = getListenerOutput({
+//     arn: 'arn:aws:elasticloadbalancing:us-east-1:303658717825:listener/app/cust-api-alb/ff89ddfab088b4c0/c549cf4d3347f66c',
+//     loadBalancerArn: alb.arn
+// });
+
+// Define the service, building and publishing our "./app/Dockerfile", and using the load balancer.
+const service1 = new awsx.ecs.FargateService("nginx", {
+    desiredCount: 1,
+    taskDefinitionArgs: {
+        containers: {
+            nginx: {
+                // image: awsx.ecs.Image.fromPath("nginx", "./app"),
+                image: 'bmccraw86/nginx-example:latest',
+                memory: 512,
+                portMappings: [listener],
+            },
+        },
+    },
+});
+const service2 = new awsx.ecs.FargateService("siege", {
+    desiredCount: 1,
+    taskDefinitionArgs: {
+        containers: {
+            nginx: {
+                // image: awsx.ecs.Image.fromPath("nginx", "./app"),
+                image: 'bmccraw86/nginx-example:latest',
+                memory: 512,
+                portMappings: [listener],
+            },
+        },
+    },
+});
+
+// Export the URL so we can easily access it.
+export const frontendURL = pulumi.interpolate `http://${listener.endpoint.hostname}/`;
+
